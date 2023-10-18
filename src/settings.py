2875181696bff73bde7bf6ee28f5069f4e715604
Alex Erastov <123257559+erastov-alex@users.noreply.github.com>
2023-10-18 10:06:11 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/erickendric/tree-dataset-of-urban-street-segmentation-leaf"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 7228288
+PREVIEW_IMAGE_ID: int = 7386589
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/urban-street-leaf"
